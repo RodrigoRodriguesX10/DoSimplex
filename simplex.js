2ca9c6e67ffbb785df70088346c11de14f019349
RodrigoRodriguesX10 <x10rodrigo@gmail.com>
2018-05-14 18:11:28 -0300

    Separado logica JS da interface

--- simplex.js
+++ simplex.js
@@ -1,25 +1,32 @@
+function Tableau(m,n){
+    this.m = Number(m) || 0;
+    this.n = Number(n) || 0;
+    this.tableau = Array.apply(null, Array(m)).map(Number.prototype.valueOf, 
+        Array.apply(null, Array(n)).map(Number.prototype.valueOf,0));
+}
+
 function pivot_on(tab, row, col) {
     var i, j, pivot, a;
-    pivot = tab.mat[row][col];
+    pivot = tab.tableau[row][col];
     console.log("Encontrou pivot: " + pivot, " linha: ", row, " coluna: ", col);
     tab.labelColumn[row-1] = tab.labelRow[col-1];
 
     for (j = 0; j < tab.n; j++)
-        tab.mat[row][j] /= pivot;
+        tab.tableau[row][j] /= pivot;
     for (i = 0; i < tab.m; i++) {
-        let multiplier = tab.mat[i][col];
+        let multiplier = tab.tableau[i][col];
         if (i == row) continue;
         for (j = 0; j < tab.n; j++) {
-            tab.mat[i][j] -= multiplier * tab.mat[row][j];
+            tab.tableau[i][j] -= multiplier * tab.tableau[row][j];
         }
     }
 }
 function find_pivot_column(tab) {
     var j, pivot_col = 1;
-    var lowest = tab.mat[0][pivot_col];
+    var lowest = tab.tableau[0][pivot_col];
     for (j = 1; j < tab.n; j++) {
-        if (tab.mat[0][j] < lowest) {
-            lowest = tab.mat[0][j];
+        if (tab.tableau[0][j] < lowest) {
+            lowest = tab.tableau[0][j];
             pivot_col = j;
         }
     } if (lowest >= 0) {
@@ -30,7 +37,7 @@ function find_pivot_row(tab, pivot_col) {
     var i, pivot_row = 0;
     var min_ratio = -1;
     for (i = 1; i < tab.m; i++) {
-        var ratio = tab.mat[i][0] / tab.mat[i][pivot_col];
+        var ratio = tab.tableau[i][0] / tab.tableau[i][pivot_col];
         if ((ratio > 0 && ratio < min_ratio) || min_ratio < 0) {
             min_ratio = ratio;
             pivot_row = i;
@@ -44,7 +51,7 @@ function add_variaveis_nao_basicas(tab) {
     for (i = 0; i < tab.m; i++) {
         tab.labelColumn.push("f" + (i + 1));
         for (j = 1; j < tab.m; j++) {
-            tab.mat[i][j + tab.n - 1] = (i == j) ? 1 : 0;
+            tab.tableau[i][j + tab.n - 1] = (i == j) ? 1 : 0;
         }
     }
     tab.labelColumn.pop(); // Remove a linha adicionada a mais (não conta a linha Z)
@@ -71,4 +78,16 @@ function simplex(tab) {
         pivot_on(tab, pivot_row, pivot_col);
         if (loop > 20) break; // Essa linha limita a quantidade de iterações, ou seja, pode ser passada por parâmetro
     }
+}
+
+function createCopy(tableau) {
+    let m = []
+    for (let i = 0; i < tableau.length; i++) {
+        let l = [];
+        for (let j = 0; j < tableau[i].length; j++) {
+            l.push(tableau[i][j]);
+        }
+        m.push(l);
+    }
+    return m;
 }
